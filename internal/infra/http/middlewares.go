3eaeb2945df0816ac8852a8a6e76a18cae706658
Namu <namulabre@gmail.com>
2026-07-12 16:25:11 +0200

    feat: add time elapsed on handler call

--- internal/infra/http/middlewares.go
+++ internal/infra/http/middlewares.go
@@ -3,12 +3,16 @@ package http
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 // LogMiddleware log the incoming request
 func LogMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s", r.Method, r.URL)
+		start := time.Now()
 		next.ServeHTTP(w, r)
+		elapsed := time.Since(start)
+
+		log.Printf("%s %s elapsed: %s", r.Method, r.URL, elapsed)
 	})
 }
